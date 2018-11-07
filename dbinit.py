7e30aa46d427df6d9874eb80c56dcf24ed56ba49
EmreEkici <emreekici407@gmail.com>
2018-11-07 21:12:26 +0300

    Update dbinit.py
    Connect to Heroku database and get the contents of the table "DUMMY".

--- dbinit.py
+++ dbinit.py
@@ -9,18 +9,22 @@ INIT_STATEMENTS = [
     "INSERT INTO DUMMY VALUES (42)",
 ]
 
-
-def initialize(url):
-    with dbapi2.connect(url) as connection:
-        cursor = connection.cursor()
-        for statement in INIT_STATEMENTS:
-            cursor.execute(statement)
-        cursor.close()
-
-
 if __name__ == "__main__":
-    url = os.getenv("DATABASE_URL")
-    if url is None:
-        print("Usage: DATABASE_URL=url python dbinit.py", file=sys.stderr)
-        sys.exit(1)
-    initialize(url)
+    dsn = "user='khxcpxyuayifiy'"
+    dsn += " password='a71d836a4a3e8c9d4030a8bd40ffec8d7e43202bf75ece49c4635701c10cd21f'"
+    dsn += " host='ec2-54-247-124-154.eu-west-1.compute.amazonaws.com'"
+    dsn += " port=5432"
+    dsn += " dbname=dd7j2nqkjb2bs9"
+    connection = dbapi2.connect(dsn)
+
+    cursor = connection.cursor()
+    for statement in INIT_STATEMENTS:
+        cursor.execute(statement)
+    statement = "SELECT * FROM DUMMY"
+    cursor.execute(statement)
+    for row in cursor:
+        num = row
+        print("%s" % num)
+    cursor.close()
+
+    connection.close();
