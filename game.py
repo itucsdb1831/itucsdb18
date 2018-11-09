30409835b1558e917459358b3e189b643f2a2dcd
EmreEkici <emreekici407@gmail.com>
2018-11-10 01:35:15 +0300

    Add votes variable to the constructor

--- game.py
+++ game.py
@@ -1,8 +1,9 @@
 class Game:
-    def __init__(self, game_id, title, genre, rating, age_restriction, price):
+    def __init__(self, game_id, title, genre, rating, votes, age_restriction, price):
         self.game_id = game_id
         self.title = title
         self.genre = genre
         self.rating = rating
+        self.votes = votes
         self.age_restriction = age_restriction
         self.price = price
