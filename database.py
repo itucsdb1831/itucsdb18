4fa04f19013737ad16c1f4b7ad963b4e19ace55c
EmreEkici <emreekici407@gmail.com>
2018-11-09 17:13:26 +0300

    Create get_games function

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 import psycopg2 as dbapi2
 from user import User
+from game import Game
 
 dsn = """user=khxcpxyuayifiy password=a71d836a4a3e8c9d4030a8bd40ffec8d7e43202bf75ece49c4635701c10cd21f
 host=ec2-54-247-124-154.eu-west-1.compute.amazonaws.com port=5432 dbname=dd7j2nqkjb2bs9"""
@@ -67,3 +68,12 @@ def add_game(game):
     connection.commit()
     cursor.close()
     connection.close()
+
+def get_games():
+    games = []
+    connection = dbapi2.connect(dsn)
+    cursor = connection.cursor()
+    for game in cursor:
+        game_ = Game(game.title, game.genre, game.age_restriciton, game.price)
+        games.append(game)
+    return games
