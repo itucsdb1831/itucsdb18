8f53503f122f42968230fb4608b3d1c05a329a1d
EmreEkici <emreekici407@gmail.com>
2018-11-10 21:38:26 +0300

    Create GameOfUser class

---
+++ game_of_user.py
@@ -0,0 +1,7 @@
+class GameOfUser:
+    def __init__(self, user_id, game_id, time_played, time_purchased):
+        self.user_id = user_id
+        self.game_id = game_id
+        self.time_played = time_played
+        self.time_purchased = time_purchased
+
