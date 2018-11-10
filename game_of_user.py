43850c0c4a370600f6fac831923f0c672561ec29
EmreEkici <emreekici407@gmail.com>
2018-11-10 22:14:31 +0300

    Add title variable

--- game_of_user.py
+++ game_of_user.py
@@ -1,7 +1,8 @@
 class GameOfUser:
-    def __init__(self, user_id, game_id, time_played, time_purchased):
+    def __init__(self, user_id, game_id, title, time_played, time_purchased):
         self.user_id = user_id
         self.game_id = game_id
+        self.title = title
         self.time_played = time_played
         self.time_purchased = time_purchased
 
