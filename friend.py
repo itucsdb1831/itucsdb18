25bc8e64f7ff072c47c45e1ba64f90dac6482fa5
EmreEkici <emreekici407@gmail.com>
2018-11-11 16:06:40 +0300

    Update friend.py
    Create friend class

---
+++ friend.py
@@ -0,0 +1,9 @@
+class Friend:
+    def __init__(self, user1_id, user2_id, date_befriended, is_blocked, is_following, num_of_shared_games, is_favourite):
+        self.user1_id = user1_id
+        self.user2_id = user2_id
+        self.date_befriended = date_befriended
+        self.is_blocked = is_blocked
+        self.is_following = is_following
+        self.num_of_shared_games = num_of_shared_games
+        self.is_favourite = is_favourite
