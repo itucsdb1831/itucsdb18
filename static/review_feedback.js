c589f9b519804b20c0c973bef47cb5727759d58f
cavitozbay <44777923+cavitozbay@users.noreply.github.com>
2018-11-27 17:07:08 +0300

    Script improved
    It was working properly with only the first review on the page. Now it is working properly.

--- static/review_feedback.js
+++ static/review_feedback.js
@@ -1,79 +1,117 @@
 function toggle_like(x) {
 	if(x.innerHTML === "Like"){
         x.innerHTML="You Liked It";
-        document.getElementById("disl_sub").disabled = true;
+        document.getElementById(x.value+"d").disabled = true;
     }
     else{
         x.innerHTML="Like";
-        document.getElementById("disl_sub").disabled = false;
+        document.getElementById(x.value+"d").disabled = false;
     }
 }
 
 function toggle_dislike(x) {
 	if(x.innerHTML === "Dislike"){
            x.innerHTML="You Disliked It";
-           document.getElementById("like_sub").disabled = true;
+           document.getElementById(x.value+"l").disabled = true;
     }
     else{
         x.innerHTML="Dislike";
-        document.getElementById("like_sub").disabled = false;
+        document.getElementById(x.value+"l").disabled = false;
     }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', function() {
 
-    document.querySelector('#like').onclick = () => {
+    document.querySelectorAll('.likes').forEach(function(button) {
+        button.onclick = function() {
+            const request = new XMLHttpRequest();
+            const like_sit = button.innerHTML;
+            request.open('POST', '/process_review_feedback/');
 
-        const request = new XMLHttpRequest();
-        const like_sit = document.querySelector('#like_sub').innerHTML;
-        request.open('POST', '/process_review_feedback/');
+            request.onload = () => {
 
-        request.onload = () => {
-
-            // Extract JSON data from request
-            const data = JSON.parse(request.responseText);
-            if (data.success) {
-                toggle_like(document.querySelector('#like_sub'));
-                document.getElementById("like_sub").disabled = false;
+                // Extract JSON data from request
+                const data = JSON.parse(request.responseText);
+                if (data.success) {
+                    toggle_like(button);
+                    document.getElementById(button.value+"d").disabled = false;
+                }
             }
-        }
-        const data = new FormData();
-        data.append('review_id', document.querySelector('#like_sub').value);
-        data.append('sit4process', "like")
-        data.append('like_sit', like_sit);
-        document.getElementById("like_sub").disabled = true;
-
-        // Send request
-        request.send(data);
-        return false;
-    };
+            const data = new FormData();
+            data.append('review_id', button.value);
+            data.append('sit4process', "like")
+            data.append('like_sit', like_sit);
+            document.getElementById(button.value+"l").disabled = true;
+    
+            // Send request
+            request.send(data);
+            return false;
+        };
+    });
 });
 
-document.addEventListener('DOMContentLoaded', () => {
 
-    document.querySelector('#dislike').onclick = () => {
+document.addEventListener('DOMContentLoaded', function() {
 
-        const request = new XMLHttpRequest();
-        const disl_sit = document.querySelector('#disl_sub').innerHTML;
-        request.open('POST', '/process_review_feedback/');
+    document.querySelectorAll('.likes').forEach(function(button) {
+        button.onclick = function() {
+            const request = new XMLHttpRequest();
+            const like_sit = button.innerHTML;
+            request.open('POST', '/process_review_feedback/');
 
-        request.onload = () => {
+            request.onload = () => {
 
-            // Extract JSON data from request
-            const data = JSON.parse(request.responseText);
-            if (data.success) {
-                toggle_dislike(document.querySelector('#disl_sub'));
-                document.getElementById("disl_sub").disabled = false;
+                // Extract JSON data from request
+                const data = JSON.parse(request.responseText);
+                if (data.success) {
+                    toggle_like(button);
+                    document.getElementById(button.value+"l").disabled = false;
+                }
             }
-        }
-        const data = new FormData();
-        data.append('review_id', document.querySelector('#disl_sub').value);
-        data.append('sit4process', "dislike")
-        data.append('disl_sit', disl_sit);
-        
-        document.getElementById("disl_sub").disabled = true;
-        // Send request
-        request.send(data);
-        return false;
-    };
+            const data = new FormData();
+            data.append('review_id', button.value);
+            data.append('sit4process', "like")
+            data.append('like_sit', like_sit);
+            document.getElementById(button.value+"l").disabled = true;
+            document.getElementById(button.value+"d").disabled = true;
+    
+            // Send request
+            request.send(data);
+            return false;
+        };
+    });
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    document.querySelectorAll('.dislikes').forEach(function(button) {
+        button.onclick = function() {
+            const request = new XMLHttpRequest();
+            const disl_sit = button.innerHTML;
+            request.open('POST', '/process_review_feedback/');
+
+            request.onload = () => {
+
+                // Extract JSON data from request
+                const data = JSON.parse(request.responseText);
+                if (data.success) {
+                    toggle_dislike(button);
+                    document.getElementById(button.value+"d").disabled = false;
+                }
+            }
+            const data = new FormData();
+            data.append('review_id', button.value);
+            data.append('sit4process', "dislike")
+            data.append('disl_sit', disl_sit);
+            document.getElementById(button.value+"d").disabled = true;
+            document.getElementById(button.value+"l").disabled = true;
+    
+            // Send request
+            request.send(data);
+            return false;
+        };
+    });
+});
+
+
