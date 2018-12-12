9787076f12c40fa55e83aa2f036dc0f4e1ff9255
EmreEkici <emreekici407@gmail.com>
2018-12-12 21:34:45 +0300

    Change variable name of games_of_losers

--- game_of_user.py
+++ game_of_user.py
@@ -1,11 +1,11 @@
 class GameOfUser:
-    def __init__(self, user_id, game_id, title, time_played, time_purchased, num_of_reviews,  num_of_screenshots, is_favourite):
+    def __init__(self, user_id, game_id, title, time_played, time_purchased, num_of_reviews,  num_of_items, is_favourite):
         self.user_id = user_id
         self.game_id = game_id
         self.title = title
         self.time_played = time_played
         self.time_purchased = time_purchased
         self.num_of_reviews = num_of_reviews
-        self.num_of_screenshots = num_of_screenshots
+        self.num_of_items = num_of_items
         self.is_favourite = is_favourite
 
