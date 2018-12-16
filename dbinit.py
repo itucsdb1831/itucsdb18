173b03c2210d197132493171f729eb308af62138
EmreEkici <emreekici407@gmail.com>
2018-12-16 13:11:45 +0300

    Minor bug fix

--- dbinit.py
+++ dbinit.py
@@ -168,19 +168,19 @@ if __name__ == "__main__":
     for statement in INIT_STATEMENTS:
         cursor.execute(statement)
 
-    # password = "asdf"
-    # hashed_password = hasher.hash(password)
-    # statement = "INSERT INTO USERS(NAME, PASSWORD, IS_ADMIN) VALUES (%s, %s, %s)"
-    # data = ("emre", hashed_password, True)
-    # cursor.execute(statement, data)
-    #
-    # statement = "INSERT INTO BALANCE_CODES VALUES (%s)"
-    # data = "1234"
-    # cursor.execute(statement, [data])
-    #
-    # statement = "INSERT INTO GAMES (TITLE, GENRE, AGE_RESTRICTION, PRICE) VALUES (%s, %s, 12, 0.00)"
-    # data = ("team fortress 2", "fps")
-    # cursor.execute(statement, data)
+    password = "asdf"
+    hashed_password = hasher.hash(password)
+    statement = "INSERT INTO USERS(NAME, PASSWORD, IS_ADMIN) VALUES (%s, %s, %s)"
+    data = ("emre", hashed_password, True)
+    cursor.execute(statement, data)
+
+    statement = "INSERT INTO BALANCE_CODES VALUES (%s)"
+    data = "1234"
+    cursor.execute(statement, [data])
+
+    statement = "INSERT INTO GAMES (TITLE, GENRE, AGE_RESTRICTION, PRICE) VALUES (%s, %s, 12, 0.00)"
+    data = ("team fortress 2", "fps")
+    cursor.execute(statement, data)
 
     connection.commit()
     cursor.close()
